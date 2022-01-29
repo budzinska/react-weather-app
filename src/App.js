437461f0c6618085f6de4ecdf9b67914dfcbb691
budzinska <budzinska.anna@gmail.com>
2022-01-29 11:40:28 +0100

    footer >> add GitHub repo url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">React Weather App</div>;
+  return (
+    <div className="App">
+      React Weather App
+      <footer>
+        This project was coded by Anna Budzińska and it is{" "}
+        <a
+          href="https://github.com/budzinska/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
