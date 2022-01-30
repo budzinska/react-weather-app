8473cc96cb4851f2f63876ead68ac23ba0684836
budzinska <budzinska.anna@gmail.com>
2022-01-30 15:12:07 +0100

    add new component WeatherDetails

---
+++ src/WeatherDetails.js
@@ -0,0 +1,32 @@
+import React from "react";
+import FormattedDate from "./FormattedDate.js";
+
+export default function WeatherDetails(props) {
+  return (
+    <div className="WeatherDetails">
+      <h3>{props.data.city}</h3>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <img src={props.data.icon} alt={props.data.description} />
+          <span className="temperature">
+            {Math.round(props.data.temperature)}
+          </span>
+          <span className="unit">°C</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: mm</li>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind}km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
